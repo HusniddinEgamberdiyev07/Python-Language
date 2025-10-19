05f272b25d4ec2c10407087034ad6b8cbb3a61f9
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2025-10-19 13:16:07 +0500

    Forgot to add lesson1.py adding now

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+a = 20
+print(a)
+# print is function and keyword
+# file name: primaryName.secondnaryName
+# primaryName is file name
+# start with letter
+# length 1-256 max 1-8
+# secondnaryName is type of file
+# second name is extension
+# length 1-4
+
+# more lines slower execution
+
+# a = 5
+# A = 10
+# name = "Husniddin"
+# print(name)
+# print("SIUT")
+# print(100)
