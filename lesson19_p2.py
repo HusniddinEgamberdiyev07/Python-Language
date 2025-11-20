30703bc589cb865e6f90e897ee5edf5b30a8a1d5
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2025-11-20 10:00:38 +0500

    while loop

--- lesson19_p2.py
+++ lesson19_p2.py
@@ -1,2 +1,9 @@
 # Looping conditions (while, for)
 # Break and continue
+
+i=0
+while i<6:
+    
+    if i != 3:
+        print("🤪->crazy person",  "😱 is getting scared by number", "( ", i, " ) ",)
+    i+=1
